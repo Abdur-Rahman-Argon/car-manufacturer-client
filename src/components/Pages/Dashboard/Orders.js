df1564b5b8adefbc3a54dac5cbe614aed6a33b4a
Abdur Rahman Argon <shehab7331@gamil.com>
2022-05-25 11:54:50 +0600

    single order load and seen

---
+++ src/components/Pages/Dashboard/Orders.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Orders = ({ order }) => {
+  const { img, ordersCountity, Price, productName } = order;
+  return (
+    <div className="card p-8 bg-base-100 shadow-xl m-1 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 my-5 mx-auto items-center">
+      <div className="w-96">
+        <img src={img} alt="" className=" w-28 mx-auto" />
+      </div>
+      <div className="w-96 mx-auto my-2">
+        <div className="w-full text-center">
+          <h1>{productName}</h1>
+          <h1>
+            price:{Price} <span className="text-sm">(per Pice)</span>
+          </h1>
+          <h1>Orders Quantity: {ordersCountity} </h1>
+        </div>
+      </div>
+      <div className="w-96 my-2 text-center">
+        <button class="btn btn-active btn-primary mx-auto ">Pay Now</button>
+      </div>
+      <div className="w-96 my-2 text-center">
+        <button class="btn w-20 btn-active btn-warning mx-auto ">Remove</button>
+      </div>
+    </div>
+  );
+};
+
+export default Orders;
